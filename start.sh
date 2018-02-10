2289e0255838c22daeea1c5a043e2664c806b9fa
root <root@ip-172-31-26-129.eu-west-1.compute.internal>
2018-02-10 18:59:18 +0000

    Add start script

---
+++ start.sh
@@ -0,0 +1,7 @@
+export CUSTOM_NETWORK_NAME=TEST
+docker network create $CUSTOM_NETWORK_NAME
+export INITIAL_ADMIN_PASSWORD=$INITIAL_ADMIN_PASSWORD_PLAIN
+export JENKINS_PWD=$INITIAL_ADMIN_PASSWORD_PLAIN
+export GERRIT_PWD=$INITIAL_ADMIN_PASSWORD_PLAIN
+docker-compose -f docker-compose.yml -f compose/elk.yml  up -d
+
